8736dcc46712c0da3440cec506c1a73e974b4998
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2016-11-10 21:06:50 +0100

    Add script to download A1 m3u files.

---
+++ download_m3u.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+# Downloads all the m3u files from A1 to local files
+
+require 'open-uri'
+
+download = open('http://epggw.a1.net/a/service.fta.m3u')
+IO.copy_stream(download, 'm3u/A1-TV_basic.m3u')
+
+download = open('http://epggw.a1.net/a/service.plus.m3u')
+IO.copy_stream(download, 'm3u/A1-TV_plus.m3u')
+
+download = open('http://epggw.a1.net/a/service.m3u')
+IO.copy_stream(download, 'm3u/A1-TV_all.m3u')
+
+download = open('http://epggw.a1.net/a/service.radio.m3u')
+IO.copy_stream(download, 'm3u/A1-TV_radio.m3u')
